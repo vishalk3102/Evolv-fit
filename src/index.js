3119cfd0334c9db20c034ba7c2de1f4a32d1d9d2
vishalk3102 <vishal.k3102@gmail.com>
2022-07-27 01:39:17 +0530

    made changes to package.json file

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
